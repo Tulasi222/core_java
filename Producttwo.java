9150dfeb2497464c4a75523db0031e0b94449ff8
Tulasi222 <tulasinarayana364@gmail.com>
2025-06-17 14:20:16 +0530

    sop

---
+++ Producttwo.java
@@ -0,0 +1,9 @@
+class Producttwo{
+public static void main(String args[]){
+System.out.println("boAt Rockerz 255 Pro+, 60HRS Battery, Fast Charge, IPX7, Dual Pairing, Low Latency, Magnetic Earbuds, Bluetooth Neckband, Wireless with Mic Earphones (Active Black)");
+System.out.println("this item Playback- Leave all charging worries at bay as the Rock rz 255 Pro+ comes with a humongous battery back up of 60 Hours");
+System.out.println("ASAP Charge- With a few minutes of ASAP Charge you can get upto 10 hours of audio time by charging them for only 10 mins");
+System.out.println("Drivers- The unbeatable boAt signature sound shines through no matter what are you playing courtesy its 10mm drivers");
+System.out.println("IP Rating- Rockerz 255 Pro+ are perfect for the outdoors and daily workout as they are IPX7 rated");
+System.out.println("Controls- Its multi-function Integrated controls lends an intuitive listening experience with volume increase and Decrease and Stop and Play buttons");
+}}
